ab0ef70a97dd0bc4c2a38622f44b28afce5f1807
Chris Thoburn <cthoburn@linkedin.com>
2017-03-20 12:38:21 -0700

    remove duplication from Relationship base class

--- addon/-private/system/relationships/state/implicit.js
+++ addon/-private/system/relationships/state/implicit.js
@@ -1,7 +1,5 @@
 /* global heimdall */
-import { assert, warn } from 'ember-data/-private/debug';
 import OrderedSet from '../../ordered-set';
-import _normalizeLink from '../../normalize-link';
 import Relationship from './relationship';
 
 const {
@@ -10,23 +8,14 @@ const {
   addRecord,
   addInternalModels,
   clear,
-  findLink,
   flushCanonical,
-  flushCanonicalLater,
   newRelationship,
-  push,
   removeCanonicalRecord,
-  removeCanonicalRecordFromInverse,
   removeCanonicalRecordFromOwn,
   removeCanonicalRecords,
   removeRecord,
-  removeRecordFromInverse,
   removeRecordFromOwn,
   removeInternalModels,
-  setHasData,
-  setHasLoaded,
-  updateLink,
-  updateMeta,
   updateRecordsFromAdapter
 } = heimdall.registerMonitor('system.relationships.state.relationship',
   'addCanonicalRecord',
@@ -34,37 +23,25 @@ const {
   'addRecord',
   'addInternalModels',
   'clear',
-  'findLink',
   'flushCanonical',
-  'flushCanonicalLater',
   'newRelationship',
-  'push',
   'removeCanonicalRecord',
-  'removeCanonicalRecordFromInverse',
   'removeCanonicalRecordFromOwn',
   'removeCanonicalRecords',
   'removeRecord',
-  'removeRecordFromInverse',
   'removeRecordFromOwn',
   'removeInternalModels',
-  'setHasData',
-  'setHasLoaded',
-  'updateLink',
-  'updateMeta',
   'updateRecordsFromAdapter'
 );
 
 export default class ImplicitRelationship extends Relationship {
   constructor(store, internalModel, inverseKey, relationshipMeta) {
     super(store, internalModel, inverseKey, relationshipMeta);
+    this.kind = 'implicit';
     heimdall.increment(newRelationship);
 
     this.members = new OrderedSet();
     this.canonicalMembers = new OrderedSet();
-
-    //This probably breaks for polymorphic relationship in complex scenarios, due to
-    //multiple possible modelNames
-    this.inverseKeyForImplicit = this.internalModel.modelName + this.key;
   }
 
   // TODO @runspired deprecate this as it was never truly a record instance
@@ -96,11 +73,6 @@ export default class ImplicitRelationship extends Relationship {
   }
 
   inverseDidDematerialize() {}
-
-  updateMeta(meta) {
-    heimdall.increment(updateMeta);
-    this.meta = meta;
-  }
 
   clear() {
     heimdall.increment(clear);
@@ -200,7 +172,7 @@ export default class ImplicitRelationship extends Relationship {
         }
         record._implicitRelationships[this.inverseKeyForImplicit].addRecord(this.record);
       }
-      this.record.updateRecordArrays();
+      this.internalModel.updateRecordArrays();
     }
     this.setHasData(true);
   }
@@ -219,29 +191,11 @@ export default class ImplicitRelationship extends Relationship {
     }
   }
 
-  removeRecordFromInverse(record) {
-    heimdall.increment(removeRecordFromInverse);
-    let inverseRelationship = record._relationships.get(this.inverseKey);
-    //Need to check for existence, as the record might unloading at the moment
-    if (inverseRelationship) {
-      inverseRelationship.removeRecordFromOwn(this.record);
-    }
-  }
-
   removeRecordFromOwn(record) {
     heimdall.increment(removeRecordFromOwn);
     this.members.delete(record);
     this.notifyRecordRelationshipRemoved(record);
     this.record.updateRecordArrays();
-  }
-
-  removeCanonicalRecordFromInverse(record) {
-    heimdall.increment(removeCanonicalRecordFromInverse);
-    let inverseRelationship = record._relationships.get(this.inverseKey);
-    //Need to check for existence, as the record might unloading at the moment
-    if (inverseRelationship) {
-      inverseRelationship.removeCanonicalRecordFromOwn(this.record);
-    }
   }
 
   removeCanonicalRecordFromOwn(record) {
@@ -270,128 +224,10 @@ export default class ImplicitRelationship extends Relationship {
     }
   }
 
-  flushCanonicalLater() {
-    heimdall.increment(flushCanonicalLater);
-    if (this.willSync) {
-      return;
-    }
-    this.willSync = true;
-    this.store._updateRelationshipState(this);
-  }
-
-  updateLink(link) {
-    heimdall.increment(updateLink);
-    warn(`You pushed a record of type '${this.record.modelName}' with a relationship '${this.key}' configured as 'async: false'. You've included a link but no primary data, this may be an error in your payload.`, this.isAsync || this.hasData , {
-      id: 'ds.store.push-link-for-sync-relationship'
-    });
-    assert(`You have pushed a record of type '${this.record.modelName}' with '${this.key}' as a link, but the value of that link is not a string.`, typeof link === 'string' || link === null);
-
-    this.link = link;
-    this.linkPromise = null;
-    this.record.notifyPropertyChange(this.key);
-  }
-
-  findLink() {
-    heimdall.increment(findLink);
-    if (this.linkPromise) {
-      return this.linkPromise;
-    } else {
-      let promise = this.fetchLink();
-      this.linkPromise = promise;
-      return promise.then((result) => result);
-    }
-  }
-
   updateRecordsFromAdapter(records) {
     heimdall.increment(updateRecordsFromAdapter);
     //TODO(Igor) move this to a proper place
     //TODO Once we have adapter support, we need to handle updated and canonical changes
     this.computeChanges(records);
   }
-
-  notifyRecordRelationshipAdded() { }
-  notifyRecordRelationshipRemoved() { }
-
-  /*
-   `hasData` for a relationship is a flag to indicate if we consider the
-   content of this relationship "known". Snapshots uses this to tell the
-   difference between unknown (`undefined`) or empty (`null`). The reason for
-   this is that we wouldn't want to serialize unknown relationships as `null`
-   as that might overwrite remote state.
-
-   All relationships for a newly created (`store.createRecord()`) are
-   considered known (`hasData === true`).
-   */
-  setHasData(value) {
-    heimdall.increment(setHasData);
-    this.hasData = value;
-  }
-
-  /*
-   `hasLoaded` is a flag to indicate if we have gotten data from the adapter or
-   not when the relationship has a link.
-
-   This is used to be able to tell when to fetch the link and when to return
-   the local data in scenarios where the local state is considered known
-   (`hasData === true`).
-
-   Updating the link will automatically set `hasLoaded` to `false`.
-   */
-  setHasLoaded(value) {
-    heimdall.increment(setHasLoaded);
-    this.hasLoaded = value;
-  }
-
-  /*
-   `push` for a relationship allows the store to push a JSON API Relationship
-   Object onto the relationship. The relationship will then extract and set the
-   meta, data and links of that relationship.
-
-   `push` use `updateMeta`, `updateData` and `updateLink` to update the state
-   of the relationship.
-   */
-  push(payload) {
-    heimdall.increment(push);
-
-    let hasData = false;
-    let hasLink = false;
-
-    if (payload.meta) {
-      this.updateMeta(payload.meta);
-    }
-
-    if (payload.data !== undefined) {
-      hasData = true;
-      this.updateData(payload.data);
-    }
-
-    if (payload.links && payload.links.related) {
-      let relatedLink = _normalizeLink(payload.links.related);
-      if (relatedLink && relatedLink.href && relatedLink.href !== this.link) {
-        hasLink = true;
-        this.updateLink(relatedLink.href);
-      }
-    }
-
-    /*
-     Data being pushed into the relationship might contain only data or links,
-     or a combination of both.
-
-     If we got data we want to set both hasData and hasLoaded to true since
-     this would indicate that we should prefer the local state instead of
-     trying to fetch the link or call findRecord().
-
-     If we have no data but a link is present we want to set hasLoaded to false
-     without modifying the hasData flag. This will ensure we fetch the updated
-     link next time the relationship is accessed.
-     */
-    if (hasData) {
-      this.setHasData(true);
-      this.setHasLoaded(true);
-    } else if (hasLink) {
-      this.setHasLoaded(false);
-    }
-  }
-
-  updateData() {}
 }
